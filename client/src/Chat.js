b0de2d16cbee5270ecef47450fa1e48afeceffa3
ErenWeng <f245986@gmail.com>
2022-09-04 20:34:40 +0800

    feat: receive message prevent re-render in strict mode

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Chat = ({socket, username, room}) => {
   const [currentMessage, setCurrentMessage] = useState('')
@@ -15,8 +15,14 @@ const Chat = ({socket, username, room}) => {
     }
   }
 
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      console.log(data)
+    })
+  }, [socket])
+
   return (
-    <div>
+    <div className="chat-window">
       <div className='chat-header'>
         <p>Live Chat</p>
       </div>
